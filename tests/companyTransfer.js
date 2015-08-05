31048a3e6b109054df83f2138b516082e57149c7
aurum <aurum.gallente@gmail.com>
2015-08-05 14:10:22 +0300

    start working on company transfer functionlity

---
+++ tests/companyTransfer.js
@@ -0,0 +1,26 @@
+// -- test for checking company transfer functionality--
+
+var user = require('./../credentials/userCredentials.js');
+var site = require('./../credentials/projectConstants.js');
+module.exports = {
+    tags: ['transfer'],
+    beforeEach: function (client) {        
+        client
+                .url(site.url)
+                .waitForElementVisible('body', 1000)
+                .assert.visible('#auth-form')
+                .setValue('input#login', user.login)
+                .setValue('input#password', user.password)
+                .click('input[type=submit]')
+                .waitForElementVisible('#taskbar', 10000)
+                .url(site.url+'/company.php?cid=237120#ui-tabs-3')
+                .pause(1000)
+                .click('.action_transfer_extra');                                
+    },
+    "check transfer dialog": function(client){
+        client
+                .pause(500)
+                .assert.visible("#addservice-form")
+                .end();
+    }    
+};
